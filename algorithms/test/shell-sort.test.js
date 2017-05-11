0dab58a00c7d582470e167e2949d3eace9e91f08
Yeramin Santana <ysantana.developer@gmail.com>
2017-05-11 10:29:36 -0400

    Added test cases for shell-sort

---
+++ algorithms/test/shell-sort.test.js
@@ -0,0 +1,11 @@
+const ShellSort = require('../shell-sort').ShellSort;
+
+describe('ShellSort Test', () => {
+    const array = [9,8,5,4,6,2,7,1,10,3];
+    const sortArray = new ShellSort(array);
+    sortArray.shellSort();
+
+    test('Should return sorted array!', () => {
+        expect(sortArray.getSortedArray()).toEqual([1,2,3,4,5,6,7,8,9,10])
+    })
+})
