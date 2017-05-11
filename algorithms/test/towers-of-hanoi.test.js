d1085da5abd77ab03f9d53cebd8848cd99276f86
Yeramin Santana <ysantana.developer@gmail.com>
2017-05-11 10:28:14 -0400

    Added moveDisk method to towers-of-hanoi and test cases

---
+++ algorithms/test/towers-of-hanoi.test.js
@@ -0,0 +1,10 @@
+const Towers = require('../towers-of-hanoi').Towers;
+
+describe('Towers of Hanoi Test', () => {
+    const towers = new Towers({totalDisks : 5, from : 'A', inter : 'B', to  : 'C'});
+    towers.start();
+
+    test('Should move all disk from tower A to C', () => {
+        expect(towers.getTowersContainer()['C']).toEqual([1,2,3,4,5])
+    })
+})
--- algorithms/towers-of-hanoi.js
+++ algorithms/towers-of-hanoi.js
@@ -1,24 +1,49 @@
 /**
  * Solving the Towers of Hanoi Puzzle
+ * Move the disks 'from' tower to distination tower following the towers of Hanoi rules
+ * @param {Object} options {totalDisks, from, inter, to}
  */
 
 class Towers {
     constructor(options) {
-        Object.assign(this,options);
+        Object.assign(this, options);
+
+        this.towersContainer = {};
+        this.towersContainer[options.from] = Array.from(new Array(options.totalDisks), (x, i) => i + 1);
+        this.towersContainer[options.inter] = [];
+        this.towersContainer[options.to] = [];
     }
+
+    getTowersContainer() {
+        return this.towersContainer;
+    }
+    
     start() {
         this.doTowers(this.totalDisks, this.from, this.inter, this.to)
     }
+
     doTowers(topN, from, inter, to) {
         if (topN === 1) {
-            console.log(`Disk 1 from ${from} to ${to}`);
+            this.moveDisk(topN, from, to);
         } else {
             this.doTowers(topN-1, from, inter, to);
-            console.log(`Disk ${topN} from ${from} to ${to}`);
+            this.moveDisk(topN, from, to);
             this.doTowers(topN-1, inter, from, to);
+        }
+    }
+
+    moveDisk(disk, from, to) {
+        //find disk
+        const diskPosition = this.towersContainer[from].indexOf(disk);
+
+        if (diskPosition !== -1) {
+           const diskValue = this.towersContainer[from][diskPosition];
+           //move the disk value
+           this.towersContainer[to].push(diskValue);
+           //remove disk from container
+           this.towersContainer[from].splice(diskPosition, 1);
         }
     }
 }
 
-const towers = new Towers({totalDisks: 3, from: 'A', inter: 'B', to :'C'});
-towers.start()
+module.exports.Towers = Towers;
